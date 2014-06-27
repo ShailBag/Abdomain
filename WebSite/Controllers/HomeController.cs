510f522fcd449ca27d6b1905b80b80248193bf58
ShailBag <shaileshbagade@gmail.com>
2014-06-27 18:11:18 +0530

    partial view added

--- WebSite/Controllers/HomeController.cs
+++ WebSite/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AbObjects.Implementation;
+using System.Collections;
 
 namespace WebSite.Controllers
 {
@@ -13,13 +15,26 @@ namespace WebSite.Controllers
             ViewBag.PersonName = "Shailesh";
             return View();
         }
-        public ActionResult AbClient()
+       
+        public ActionResult ClientList()
         {
-            AbObjects.IPerson client = new AbObjects.Implementation.Person();
-            client.FirstName = "Shailesh";
-            client.LastName = "Bagade";
+            Person client1 = new Person();
+            client1.FirstName = "Shailesh";
+            client1.LastName = "Bagade";
+            client1.PersonId = 1;
+            client1.Location = "Navi Mumbai";
 
-            return View(client);
+            Person client2 = new Person();
+            client2.FirstName = "V";
+            client2.LastName = "Bagade";
+            client2.PersonId = 2;
+            client2.Location = "Navi Mumbai";
+
+            List<Person> clientList = new List<Person>();
+            clientList.Add(client1);
+            clientList.Add(client2);
+
+            return View(clientList);
         }
     }
 }
